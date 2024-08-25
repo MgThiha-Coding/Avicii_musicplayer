29373281927e6a762ac53b1f01a7218b3b217d89
Nathan James <MgThiha>
2024-08-26 00:30:03 +0630

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,20 @@
-
-import 'package:avicii_/screen/home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:avicii_/screen/home.dart';
 
-void main() => runApp(
-  ProviderScope(child: MyApp())
-);
+void main() {
+  runApp(
+    ProviderScope(child: MyApp()),
+  );
+}
+
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  
   @override
   Widget build(BuildContext context) {
+  
     return MaterialApp(
       theme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
